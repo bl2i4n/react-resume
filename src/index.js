f4621594adbea2fef4043ff3f0133b79e4b51054
bl2i4n <bl2i4n@yahoo.com>
2017-11-30 15:45:07 -0800

    feat: Add webpack for json-loader to work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 const json = require('./resume.json');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App/>, document.getElementById('root'));
