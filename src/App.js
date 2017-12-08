ff5b27e59025677bf8fcd829662616042929b7f4
bl2i4n <bl2i4n@yahoo.com>
2017-12-07 16:24:31 -0800

    bug: clean up reverts from bad commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import ReactJsonSchema from 'react-json-schema';
 import logo from './logo.svg';
 import './App.css';
-
-const schema = {
-  "component": "basics",
-    "name": "Brian Dela Cruz",
-    "label": "Software Engineer",
-    "picture": "",
-    "email": "briandelacruz408@gmail.com",
-    "phone": "(408)806-8714",
-    "website": "https://briandelacruz.com",
-    "summary": "Graduated from Bloc.io's full stack apprenticeship that teaches both frontend and backend technologies. Grew up in the 90's and have always been fascinated by unique user interfaces and emerging technologies. From Tamogochi's, Nintendo 64's, and T-mobile sidekicks, new tech has always amazed me. Seeing how people, communities and habits change through tech is an enlightening experience. I want to be able to use software and the web to assist business, people, and communities at large.",
-    "location": {
-      "address": "2712 Broadway St",
-      "postalCode": "CA 94115",
-      "city": "San Francisco",
-      "countryCode": "US",
-      "region": "California"
-    },
-    "profiles": [
-      {
-        "network": "Linkedin",
-        "username": "briandelacruz408",
-        "url": ""
-      },
-      {
-        "network": "Github",
-        "username": "bl2i4n",
-        "url": ""
-      }
-    ]
-  }
-
-  const view = new ReactJsonSchema();
-  view.setComponentMap({ basics, profiles });
-
 
 class App extends Component {
 
